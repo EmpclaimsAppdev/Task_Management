2a738a392634100b9886419288f79ece4d5630b9
EmpclaimsAppdev <115637099+EmpclaimsAppdev@users.noreply.github.com>
2022-10-13 10:40:06 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Addproject from './component/addproject.js';
+// import Allaccount from './component/allaccount';
+
+function App() {
+  return (
+    <div>
+      <Addproject />
+    </div>
+  );
+}
+
+export default App;
